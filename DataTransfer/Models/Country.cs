69de9a3d2accd366ae2b6eba21ec64f7b67e685b
ptCady1 <ptCady0@gmail.com>
2025-03-01 15:43:39 -0600

    Add files via upload

---
+++ DataTransfer/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataTransfer.Models
+{
+    public class Country
+    {
+        public string countryID { get; set; } = string.Empty;
+        public string countryName { get; set; } = string.Empty;
+        public Games Games { get; set; } = null!;
+        public SportType SportType { get; set; } = null!;
+        public string Flag {  get; set; } = string.Empty;
+    }
+}
---
+++ DataTransfer/Models/Sport-Type.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataTransfer.Models
+{
+    public class SportType
+    {
+        public string SportTypeID { get; set; } = string.Empty;
+        public string SportName { get; set;} = string.Empty;
+    }
+}
