21c28f8d9df76975344b6cfbae267b912a8f8c94
ptCady1 <ptCady0@gmail.com>
2025-03-06 12:24:03 -0600

    Add files via upload

---
+++ Razor-Template/Models/CountryViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignments.Models
+{
+    public class CountryViewModel
+    {
+        public string ActiveConf { get; set; } = "all";
+        public string ActiveDiv { get; set; } = "all";
+
+        public Country Country { get; set; }
+
+        public List<Country> Countries { get; set;} = new List<Country>();
+
+        public List<Games> Game { get; set; } = new List<Games>();
+        public List<SportType> SportsTypes { get; set; } = new List<SportType>();
+
+        public string CheckActiveConf(string c)=> c.ToLower() == ActiveConf.ToLower() ? "active" : "";
+
+        public string CheckActiveDiv(string d) => d.ToLower() == ActiveDiv.ToLower() ? "active" : "";
+    }
+}
