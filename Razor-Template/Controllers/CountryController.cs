2c394dcae94f13a3f0eef4e209da06b9a895a128
ptCady1 <ptCady0@gmail.com>
2025-03-06 12:22:15 -0600

    Add files via upload

---
+++ Razor-Template/Controllers/CountryController.cs
@@ -0,0 +1,56 @@
+﻿using Assignments.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Assignments.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CountryController(CountryContext ctx) => context = ctx;
+
+        public CountryContext context { get; set; }
+
+        public ViewResult TheGames(CountryViewModel model)
+        {
+
+            model.Game = context.Game.ToList();
+            model.SportsTypes = context.SportsTypes.ToList();
+
+            IQueryable<Country> query = context.Countries.OrderBy(c => c.countryName);
+            if (model.ActiveConf != "all")
+                query = query.Where(
+                    c => c.Games.GamesID.ToLower() == model.ActiveConf.ToLower());
+            if (model.ActiveDiv != "all")
+                query = query.Where(
+                    c => c.SportType.SportTypeID.ToLower() == model.ActiveDiv.ToLower());
+
+            model.Countries = query.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public RedirectToActionResult Details(CountryViewModel model)
+        {
+           // Utility.LogCountryClick(model.Country.countryID);
+            TempData["ActiveConf"] = model.ActiveConf;
+            TempData["ActiveDiv"] = model.ActiveDiv;
+            return RedirectToAction("Details", new { ID = model.Country.countryID });
+
+        }
+        [HttpGet]
+        public ViewResult Details(string id)
+        {
+            var country = context.Countries
+                //Country = context.Countries
+                .Include(c => c.Games)
+                .Include(c => c.SportType)
+                .FirstOrDefault(c => c.countryID == id) ?? new Country();
+            return View(country);
+        }
+            //ActiveConf = TempData.Peek("ActiveConf").ToString(),
+            // ActiveDiv = TempData.Peek("ActiveDiv").ToString()
+
+    }
+}
