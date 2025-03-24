2ade2566241a1503df84f5ec0f28ee56d002a234
ptCady1 <ptCady0@gmail.com>
2025-03-24 17:19:39 -0500

    Add files via upload

---
+++ Data-Transfer/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+using DataTransfer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Net.Sockets;
+
+namespace DataTransfer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(CountryContext ctx) => context = ctx;
+
+        private CountryContext context;
+
+        public ViewResult Index(CountryViewModel model)
+        {
+            var session = new CountrySession(HttpContext.Session);
+            session.SetActiveConf(model.ActiveConf);
+            session.SetActiveDiv(model.ActiveDiv);
+
+            model.Game = context.Game.ToList();
+            model.SportsTypes = context.SportsTypes.ToList();
+
+            IQueryable<Country> query = context.Countries.OrderBy(c => c.countryName);
+            if (model.ActiveConf != "all")
+                query = query.Where(
+                    c => c.Games.GamesID.ToLower() == model.ActiveConf.ToLower());
+            if (model.ActiveDiv != "all")
+                query = query.Where(
+                    c => c.SportType.SportTypeID.ToLower() == model.ActiveDiv.ToLower());
+
+            model.Countries = query.ToList();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public RedirectToActionResult Details(CountryViewModel model)
+        {
+            // Utility.LogCountryClick(model.Country.countryID);
+            TempData["ActiveConf"] = model.ActiveConf;
+            TempData["ActiveDiv"] = model.ActiveDiv;
+            return RedirectToAction("Details", new { ID = model.Country.countryID });
+
+        }
+        [HttpGet]
+        public ViewResult Details(string id)
+        {
+            var session = new CountrySession(HttpContext.Session);
+            var model = new CountryViewModel
+            {
+             Country = context.Countries
+            .Include(c => c.Games)
+            .Include(c => c.SportType)
+            .FirstOrDefault(c => c.countryID == id) ?? new Country(),
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv()
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data-Transfer/Models/SessionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace DataTransfer.Models
+{
+    public static class SessionExtensions
+    {
+        public static void SetObject<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T? GetObject<T>(this ISession session, string key)
+        {
+            var json = session.GetString(key);
+            return(string.IsNullOrEmpty(json)) ? default(T) : JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
