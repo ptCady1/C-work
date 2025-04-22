2f19e825fbf73360fe2c4aae21a938c4086b026a
ptCady1 <ptCady0@gmail.com>
2025-04-22 15:32:51 -0500

    Add files via upload

---
+++ ToDoList/Views/Home/Components/StatusLink/Default.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Views.Home.Components.StatusLink
+{
+    public class Default : ViewComponent
+    {
+        public IViewComponentResult Invoke(string id, string name)
+        {
+            Status status = new Status
+            {
+                StatusId = id,
+                Name = name
+            };
+            return View(status);
+        }
+    }
+}
